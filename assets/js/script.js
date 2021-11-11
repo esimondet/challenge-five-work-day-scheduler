a3303c6cf277dbf5000f147eb0b906a1b1673ded
Edison Simondet <edison.simondet@gmail.com>
2021-11-11 17:02:57 -0600

    js loadPage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,52 @@
-var currentDay = document.querySelector("#currentDay");
+var timeArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 
-var currentDay = function () {
-    document.getElementById("currentDay"). innerHTML = moment().format("dddd" + ", " + "MMMM Do");
+var loadPage = function () {
+    $("#currentDay").html(moment().format("dddd" + ", " + "MMMM Do"));
+
+    for (let i = 0; i < timeArray.length; i++) {
+
+        var scheduleDiv = $("<div></div>");
+        scheduleDiv.addClass("row");
+
+        var timeDiv = $("<div></div>");
+        timeDiv.addClass("hour");
+        timeDiv.html(timeArray[i]);
+
+        var textDiv = $("<div></div>");
+        textDiv.addClass("textarea time-block description");
+
+        if ((i+9) < moment().format("HH")) {
+            textDiv.addClass("past");
+        } else if ((i+9) > moment().format("HH")) {
+            textDiv.addClass("future");
+        } else {
+            textDiv.addClass("present");
+        }
+
+        var saveButton = $("<button></button>");
+        saveButton.addClass("saveBtn")
+
+        var saveIcon = $("<i></i>")
+        saveIcon.addClass("fas fa-save")
+
+        scheduleDiv.append(timeDiv);
+        scheduleDiv.append(textDiv);
+        scheduleDiv.append(saveButton);
+        saveButton.append(saveIcon);
+        $("#schedule").append(scheduleDiv);
+    };
 }
 
-//currentDay temp call, once made place call in when "var loadSchedule = function ()" along with localStorage
-currentDay();
+
+
+loadPage();
+
+/*
+To Do:
+
+Set up localStorage
+Have page load tasks on open
+
+Editable/ saveable tasks
+
+*/
