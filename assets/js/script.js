da5f326f862029dce51a2f1b314549b16910efb8
Edison Simondet <edison.simondet@gmail.com>
2021-11-11 18:35:11 -0600

    added local storage and button click functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,28 @@
-var timeArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+var timeArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+var emptySchedule = {
+    "schedule": []
+}
+
+var storeSchedule = emptySchedule;
+
+//bugggss?
+//possibly only saving first item to local storage
+//not loading items from local storage
 
 var loadPage = function () {
     $("#currentDay").html(moment().format("dddd" + ", " + "MMMM Do"));
+
+    var localSchedule = localStorage.getItem("schedule");
+    if (localSchedule != null) {
+        storeSchedule = JSON.parse(localSchedule);
+    }
 
     for (let i = 0; i < timeArray.length; i++) {
 
         var scheduleDiv = $("<div>");
         scheduleDiv.addClass("row");
+        scheduleDiv.attr("id", timeArray[i]);
 
         var timeDiv = $("<div>");
         timeDiv.addClass("hour");
@@ -15,9 +31,9 @@ var loadPage = function () {
         var textDiv = $("<div>");
         textDiv.addClass("time-block");
 
-        if ((i+9) < moment().format("HH")) {
+        if ((i + 9) < moment().format("HH")) {
             textDiv.addClass("past");
-        } else if ((i+9) > moment().format("HH")) {
+        } else if ((i + 9) > moment().format("HH")) {
             textDiv.addClass("future");
         } else {
             textDiv.addClass("present");
@@ -25,12 +41,17 @@ var loadPage = function () {
 
         var textInput = $("<textarea>");
         textInput.addClass("textarea description");
+        // checking in schedule array to find if entry with hour already exists 
+        if (storeSchedule["schedule"].filter(i => i.hour === timeArray[i]).length > 0) {
+            textInput.text(storeSchedule["schedule"].filter(i => i.hour === timeArray[i])[0].meetingText);
+        }
 
         var saveButton = $("<button>");
-        saveButton.addClass("saveBtn")
+        saveButton.addClass("saveBtn");
+        saveButton.attr("id", "save");
 
-        var saveIcon = $("<i>")
-        saveIcon.addClass("fas fa-save")
+        var saveIcon = $("<i>");
+        saveIcon.addClass("fas fa-save");
 
         scheduleDiv.append(timeDiv);
         scheduleDiv.append(textDiv);
@@ -38,28 +59,22 @@ var loadPage = function () {
         scheduleDiv.append(saveButton);
         saveButton.append(saveIcon);
         $("#schedule").append(scheduleDiv);
-
-        // saveButton.addEventListener("click", function () {addSchedule(timeArray[i])});
     };
-}
 
-var storeSchedule = {
-    "schedule": []
-}
+    $("#save").on("click", function () {
+        var hour = $(this).parent().attr("id");
+        var meetingText = $(this).parent().find("textarea").val();
 
-var addSchedule = function () {
-    
+        if (storeSchedule["schedule"].filter(i => i.hour === hour).length > 0) {
+            // check each element to find index of hour
+            var index = storeSchedule["schedule"].findIndex(selectedHour => selectedHour === hour);
+            // remove stored data...
+            storeSchedule["schedule"].remove(index)
+        }
+        //...then replace with new data
+        storeSchedule["schedule"].push({ "hour": hour, "meeting": meetingText });
+        localStorage.setItem("schedule", JSON.stringify(storeSchedule));
+    });
 }
 
 loadPage();
-
-
-/*
-To Do:
-
-Set up localStorage
-Have page load tasks on open
-
-Editable/ saveable tasks
-
-*/
